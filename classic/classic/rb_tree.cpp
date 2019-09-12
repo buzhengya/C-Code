e538988475dd8d09b06ab4385dd56440529dd301
feng.wang <feng.wang@mokun.net>
2019-09-12 17:31:38 +0800

    rbtree poll algorithm

--- classic/classic/rb_tree.cpp
+++ classic/classic/rb_tree.cpp
@@ -4,14 +4,159 @@
 
 using namespace std;
 
-inline bool IsRed(RBTreeNode * pNode)
+enum PollOrder
+{
+	PreOrder = 0,
+	InOrder = 1,
+	PostOrder = 2
+};
+
+bool IsRed(RBTreeNode * pNode)
 {
 	return pNode->bRed;
 }
 
-inline bool DeleteNode(RBTree * pTree, RBTreeNode * pNode)
+bool IsLeaf(RBTree * pTree, RBTreeNode * pNode)
+{
+	return pNode->pLeft == pTree->pLeaf && pNode->pRight == pTree->pLeaf;
+}
+
+vector<RBTreeNode*> vecNodeAddr;
+bool CollectNodeAddr(RBTree * pTree, RBTreeNode * pNode)
+{
+	
+}
+
+int32 nMaxBlack = 0;
+int32 nCurBlack = 0;
+bool JudgeRBTree(RBTree * pTree, RBTreeNode * pNode)
+{
+	// root is black
+
+	// red father and son
+
+	// binary tree
+
+	// leaf black length
+}
+
+void PrintNodeStatus(RBTree * pTree, RBTreeNode * pNode)
 {
 
+}
+
+int32 nLastKey = -1;
+bool PrintNode(RBTree * pTree, RBTreeNode * pNode)
+{
+	cout << pNode->nKey << endl;
+
+	int32 nTmp = nLastKey;
+	nLastKey = pNode->nKey;
+	if (nLastKey = -1)
+	{
+		nLastKey = pNode->nKey;
+		return true;
+	}
+	return nTmp < nLastKey;
+}
+
+void InitBeforePoll()
+{
+	// print
+	nLastKey = -1;
+
+	// judge rbtree
+	nMaxBlack = 0;
+	nCurBlack = 0;
+
+	// collect addr pointer
+	vecNodeAddr.clear();
+}
+
+typedef bool(*DealNode)(RBTree *, RBTreeNode *);
+
+bool PollRBTree(RBTree * pTree, DealNode func, PollOrder eOrder)
+{
+	if (pTree->pRoot == pTree->pLeaf)
+	{
+		return;
+	}
+
+	InitBeforePoll();
+
+	vector<RBTreeNode*> vecNode;
+	vecNode.push_back(pTree->pRoot);
+	RBTreeNode * pTop = pTree->pRoot;
+	RBTreeNode * pLast = nullptr;
+
+	while (!vecNode.empty())
+	{
+		pTop = vecNode[vecNode.size() - 1];
+
+		// last is left 
+		if (pTop->pLeft == pLast)
+		{
+			if (eOrder == InOrder)
+			{
+				if (func(pTree, pTop) == false)
+				{
+					PrintNodeStatus(pTree, pTop);
+					return false;
+				}
+			}
+
+			// right is leaf
+			if (pTop->pRight == pTree->pLeaf)
+			{
+				pLast = pTop->pRight;
+			}
+			else
+			{
+				vecNode.push_back(pTop->pRight);
+				pLast = pTop;
+			}
+		}
+		// last is right
+		else if (pTop->pRight == pLast)
+		{
+			if (eOrder == PostOrder)
+			{
+				if (func(pTree, pTop) == false)
+				{
+					PrintNodeStatus(pTree, pTop);
+					return false;
+				}
+			}
+
+			vecNode.pop_back();
+			//deal cur call back
+			pLast = pTop;
+		}
+		// last not left and right
+		else
+		{
+			if (eOrder == PreOrder)
+			{
+				if (func(pTree, pTop) == false)
+				{
+					PrintNodeStatus(pTree, pTop);
+					return false;
+				}
+			}
+
+			if (pTop->pLeft == pTree->pLeaf)
+			{
+				pLast = pTop->pLeft;
+			}
+			else
+			{
+				vecNode.push_back(pTop->pLeft);
+				pLast = pTop;
+			}
+		}
+	}
+
+	return true;
 }
 
 RBTree * RBTreeInit()
@@ -32,38 +177,7 @@ RBTree * RBTreeInit()
 void RBTreeRelease(RBTree * pTree)
 {
 	// empty
-	if (pTree->pRoot == pTree->pLeaf)
-	{
-		return;
-	}
 
-	vector<RBTreeNode*> vecNode;
-	RBTreeNode * pTop = pTree->pRoot;
-	while (pTop != pTree->pLeaf)
-	{
-		vecNode.push_back(pTop);
-		pTop = pTop->pLeft;
-	}
-
-	RBTreeNode * pLast = pTop->pLeft;
-	while (!vecNode.empty())
-	{
-		pTop = vecNode[vecNode.size() - 1];
-		vecNode.pop_back();
-
-		if (pTop->pLeft == pLast)
-		{
-			pTo
-		}
-		else if (pTop->pRight == pLast)
-		{
-
-		}
-		else
-		{
-			assert(false);
-		}
-	}
 
 }
 
