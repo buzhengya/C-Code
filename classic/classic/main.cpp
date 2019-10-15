eca93a9482618b0c34b14160ca0a90ef598e83af
feng.wang <feng.wang@mokun.net>
2019-10-15 21:22:31 +0800

    rbtree correct

--- classic/classic/main.cpp
+++ classic/classic/main.cpp
@@ -1,8 +1,10 @@
 #include "rbtree/rb_tree_test.h"
 #include "mem_pool/pool_test.h"
+#include "event/event_test.h"
 
 int main()
 {
-	TestPool(2000);
+	TestRBTree(1000);
+	//TestEventTimer(10000);
 	return 0;
 }
